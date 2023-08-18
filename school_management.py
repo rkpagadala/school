c0bbf6e19bcc473da11a9361b9b16d974c808819
Rani-01 <109415982+Rani-01@users.noreply.github.com>
2023-08-18 22:03:44 +0530

    Update school_management.py
    used sqlalchemy for database framework

--- school_management.py
+++ school_management.py
@@ -1,32 +1,58 @@
 # school_management.py
 
-import sqlite3
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Create the database and table
-def create_database():
-    connector = sqlite3.connect('school_db_test.db')
-    cursor = connector.cursor()
+# Define the SQLite engine
+engine = create_engine('sqlite:///school_management_data.db', echo=True)  # Change the database name if needed
 
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS SCHOOL_MANAGEMENT (
-            STUDENT_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            NAME TEXT,
-            EMAIL TEXT,
-            PHONE_NO TEXT,
-            GENDER TEXT,
-            DOB TEXT,
-            STREAM TEXT
-        )
-    ''')
+# Create a Session class
+Session = sessionmaker(bind=engine)
 
-    connector.commit()
-    connector.close()
+# Rest of your code...
+
+
+
+# Define the Student model
+Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = 'students'
+    admission_no = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    phone_no = Column(String)
+    gender = Column(String)
+    dob = Column(Date)
+    stream = Column(String)
+
+    def update_data(self, new_data):
+        self.name = new_data['name']
+        self.email = new_data['email']
+        self.phone_no = new_data['phone_no']
+        self.gender = new_data['gender']
+        self.dob = new_data['dob']
+        self.stream = new_data['stream']
 
 # Other functions and configurations here
 
+# Define the update_record function
 def update_record(admission_no, new_data):
-    connector = sqlite3.connect('school_db_test.db')
-    cursor = connector.cursor()
+    session = Session()
+    student = session.query(Student).get(admission_no)
+    
+    if student:
+        student.update_data(new_data)
+        session.commit()
+        session.close()
+        return True
+    else:
+        session.close()
+        return False
+
 
     try:
         cursor.execute('UPDATE SCHOOL_MANAGEMENT SET NAME=?, EMAIL=?, PHONE_NO=?, GENDER=?, DOB=?, STREAM=? WHERE STUDENT_ID=?',
